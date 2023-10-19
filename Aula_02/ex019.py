79f311b01805e9cab85d479b94e86b408543760c
violett <jose>
2023-10-19 12:26:23 -0300

    import da pasta pai

--- Aula_02/ex019.py
+++ Aula_02/ex019.py
@@ -4,8 +4,9 @@ tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da
 tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18
 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem
 compradas e o preço total.
-
 '''
+
+import __init__
 
 from funcoes import *
 
